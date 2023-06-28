50a42eb86a9a10cd23ff4b88726265a360c176b6
Paulo Matos <contato.paulomatos@gmail.com>
2023-06-28 12:14:11 -0300

    Added authentication: using cookies and showing user info - github only

---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import { getUser } from '@/lib/auth'
+import Image from 'next/image'
+
+export function Profile() {
+  const { name, avatarUrl } = getUser()
+
+  return (
+    <div className="flex items-center gap-3 text-left">
+      <Image
+        src={avatarUrl}
+        width={40}
+        height={40}
+        alt=""
+        className="h-10 w-10 rounded-full"
+      />
+
+      <p className="max-w-[200px] text-sm leading-snug">
+        {name}
+        <a href="#" className="block text-red-300 hover:text-red-200">
+          Logout
+        </a>
+      </p>
+    </div>
+  )
+}
