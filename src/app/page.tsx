80ede94f5c0437cafb3e72acd60ad5634573c104
Paulo Matos <contato.paulomatos@gmail.com>
2023-07-29 23:22:54 -0300

    Added new memory creation and memories page list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,67 @@
+import dayjs from 'dayjs'
+import { api } from '@/lib/api'
+import { cookies } from 'next/headers'
 import { EmptyMemories } from '@/components/EmptyMemories'
+import Image from 'next/image'
+import Link from 'next/link'
+import { ArrowRight } from 'lucide-react'
 
-export default function Home() {
-  return <EmptyMemories />
+interface Memory {
+  id: string
+  exerpt: string
+  coverUrl: string
+  createdAt: string
+}
+
+export default async function Home() {
+  const isAuthenticated = cookies().has('token')
+
+  if (!isAuthenticated) {
+    return <EmptyMemories />
+  }
+
+  const token = cookies().get('token')?.value
+  const response = await api.get('/memories', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  const memories: Memory[] = response.data
+
+  if (memories.length === 0) {
+    return <EmptyMemories />
+  }
+
+  return (
+    <div className="flex flex-col gap-10 p-8">
+      {memories.map((memory) => {
+        return (
+          <div key={memory.id} className="space-y-4">
+            <time className="-ml-12 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
+              {dayjs(memory.createdAt).format('MMMM D, YYYY')}
+            </time>
+            <Image
+              src={memory.coverUrl}
+              alt=""
+              width={592}
+              height={280}
+              className="aspect-video w-full rounded-lg object-cover"
+            />
+            <p className="text-lg leading-relaxed text-gray-100">
+              {memory.exerpt}
+            </p>
+
+            <Link
+              href={`/memories/${memory.id}`}
+              className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
+            >
+              Read More
+              <ArrowRight className="h-4 w-4" />
+            </Link>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
--- src/components/EmptyMemories.tsx
+++ src/components/EmptyMemories.tsx
@@ -1,6 +1,6 @@
 export function EmptyMemories() {
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex flex-1 items-center justify-center p-16">
       <p className="w-[330px] text-center leading-relaxed">
         You haven&apos;t created any events yet, let&apos; start{' '}
         <a href="" className="underline hover:text-gray-50">
